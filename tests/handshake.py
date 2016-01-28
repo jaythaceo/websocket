7bfc26876138f09826c1f7e4da9684cef20099e2
jaythaceo <jaythaceo@gmail.com>
2016-01-27 23:26:29 -0500

    [test] Added webserver tests

---
+++ tests/handshake.py
@@ -0,0 +1,12 @@
+import _bootstrap_
+from websocket import *
+
+handler = DummyWebsocketHandler()
+
+
+
+pairs = [
+  # Key                         # Response
+  ('zyjFH2rQwrTtNFk5lwEMQg==', '2hnZADGmT/V1/w1GJYBtttUKASY='),
+  ('XJuxlsdq0QrVyKwA/D9D5A==', 'tZ5RV3pw7nP9cF+HDvTd89WJKj8=')
+]
